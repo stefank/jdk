0759bf09e0547ed2be4fb6bcfaa53ae945829fa6
Stefan Karlsson <stefank@openjdk.org>
2021-10-12 09:16:42 +0200

    Move handle out of CHeap allocated hashtable

--- src/hotspot/share/cds/unregisteredClasses.cpp
+++ src/hotspot/share/cds/unregisteredClasses.cpp
@@ -34,6 +34,7 @@
 #include "memory/oopFactory.hpp"
 #include "memory/resourceArea.hpp"
 #include "oops/instanceKlass.hpp"
+#include "oops/oopHandle.inline.hpp"
 #include "runtime/handles.hpp"
 #include "runtime/javaCalls.hpp"
 #include "services/threadService.hpp"
@@ -72,7 +73,7 @@ InstanceKlass* UnregisteredClasses::load_class(Symbol* name, const char* path, T
 }
 
 class URLClassLoaderTable : public ResourceHashtable<
-  Symbol*, Handle,
+  Symbol*, OopHandle,
   137, // prime number
   ResourceObj::C_HEAP> {};
 
@@ -103,13 +104,14 @@ Handle UnregisteredClasses::get_url_classloader(Symbol* path, TRAPS) {
   if (_url_classloader_table == NULL) {
     _url_classloader_table = new (ResourceObj::C_HEAP, mtClass)URLClassLoaderTable();
   }
-  Handle* url_classloader_ptr = _url_classloader_table->get(path);
+  OopHandle* url_classloader_ptr = _url_classloader_table->get(path);
   if (url_classloader_ptr != NULL) {
-    return *url_classloader_ptr;
+    return Handle(THREAD, url_classloader_ptr->resolve());
   } else {
     Handle url_classloader = create_url_classloader(path, CHECK_NH);
-    _url_classloader_table->put(path, url_classloader);
+    OopHandle url_classloader_oh(Universe::vm_global(), url_classloader());
+    _url_classloader_table->put(path, url_classloader_oh);
     path->increment_refcount();
-    return url_classloader;
+    return Handle(THREAD, url_classloader_oh.resolve());
   }
 }
