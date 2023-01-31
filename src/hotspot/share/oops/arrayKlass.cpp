ab1993c19e768c9ed1e18ca8e7939e346bdd575a
Stefan Karlsson <stefank@openjdk.org>
2023-01-31 09:39:46 +0100

    Retry removing null casts

--- src/hotspot/share/oops/arrayKlass.cpp
+++ src/hotspot/share/oops/arrayKlass.cpp
@@ -110,7 +110,7 @@ void ArrayKlass::complete_create_array_klass(ArrayKlass* k, Klass* super_klass, 
   // java.base is defined.
   assert((module_entry != nullptr) || ((module_entry == nullptr) && !ModuleEntryTable::javabase_defined()),
          "module entry not available post " JAVA_BASE_NAME " definition");
-  oop module = (module_entry != nullptr) ? module_entry->module() : (oop)nullptr;
+  oop module = (module_entry != nullptr) ? module_entry->module() : nullptr;
   java_lang_Class::create_mirror(k, Handle(THREAD, k->class_loader()), Handle(THREAD, module), Handle(), Handle(), CHECK);
 }
 
